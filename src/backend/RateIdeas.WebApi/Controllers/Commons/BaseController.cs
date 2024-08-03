0fc30107c3eefb0a19d7ddcfc3143165eb640a2f
muqimjon <muqimjon.mr@gmail.com>
2024-08-03 13:00:56 +0500

    🌐 Enabled CORS

--- src/backend/RateIdeas.WebApi/Controllers/Commons/BaseController.cs
+++ src/backend/RateIdeas.WebApi/Controllers/Commons/BaseController.cs
@@ -1,9 +1,11 @@
 ﻿global using EcoLink.WebApi.Controllers.Commons;
 global using MediatR;
 global using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
 
 namespace EcoLink.WebApi.Controllers.Commons;
 
+[EnableCors("AllowAll")]
 [Route("api/[controller]")]
 [ApiController]
 public class BaseController : ControllerBase
